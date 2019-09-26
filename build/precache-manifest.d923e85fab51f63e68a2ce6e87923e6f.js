d3c681edae65f56c0f1e46de9e30890f47726df1
matthew <mattgia3@gmail.com>
2019-09-25 21:46:27 -0400

    add react app!

---
+++ build/precache-manifest.d923e85fab51f63e68a2ce6e87923e6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fc5697a1f490dbd083fe87032bf812f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e20e60c591abfb87f03",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "78bebf3c783bf636bc01",
+    "url": "/static/js/2.cfa62ad3.chunk.js"
+  },
+  {
+    "revision": "4e20e60c591abfb87f03",
+    "url": "/static/js/main.fbf60f42.chunk.js"
+  },
+  {
+    "revision": "ee75dd08df5c8c8b3341",
+    "url": "/static/js/runtime-main.b67ccf64.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        react app on github pages!
+      </header>
+    </div>
+  );
+}
+
+export default App;
